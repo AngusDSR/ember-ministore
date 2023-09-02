59acb3063f33f82b42a0ce7aa4a0ddc94d006795
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-09-02 11:14:17 +0100

    set up basic controller

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,3 +1,19 @@
 import Controller from '@ember/controller';
 
-export default class CartController extends Controller {}
+export default class CartController extends Controller {
+  get subtotal() {
+    return this.model.reduce((acc, item) => {
+      return acc + item.price;
+    }, 0);
+  }
+
+  get discount() {
+    // make dynamic
+    return this.subtotal * 0.1;
+  }
+
+  get total() {
+    return this.subtotal - this.discount;
+  }
+
+}
--- app/routes/cart.js
+++ app/routes/cart.js
@@ -5,4 +5,17 @@ export default class CartRoute extends Route {
     const items = [{ price: 10 }, { price: 5 }, { price: 15 }, { price: 20 }];
     return items;
   }
+
+  // override the setupController hook to set the subtotal, discount, and total properties on the controller:
+  // setupController(controller, model) {
+  //   super.setupController(controller, model);
+  //   const subtotal = model.reduce((acc, item) => {
+  //     return acc + item.price;
+  //   }, 0);
+  //   controller.set(
+  //     'subtotal',
+  //     subtotal
+  //   )
+  // };
+
 }
